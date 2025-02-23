5ea6947b36bdf7676a247e094e274f317231c026
Sami Pasquali <5637087+samp1044@users.noreply.github.com>
2025-02-23 12:16:24 +0100

    feat: finished full export for given user

--- src/export.py
+++ src/export.py
@@ -97,10 +97,10 @@ all_users = response.json()
 users_export_path = Path(export_path, "users")
 users_export_path.mkdir(parents=True)
 
-with (users_export_path / "users.json").open("w") as f:
+with (users_export_path / "users.json").open("w", encoding="utf8") as f:
     json.dump(all_users, f)
 
-with (users_export_path / "me.json").open("w") as f:
+with (users_export_path / "me.json").open("w", encoding="utf8") as f:
     json.dump(current_user, f)
 
 ## Add profile pictures and export user data
@@ -122,6 +122,47 @@ for user in all_users:
             f.write(chunk)
 
 # -------------------------------------------
+#  Export custom emoji
+# -------------------------------------------
+
+emojis_export_path = Path(export_path, "emojis")
+emojis_export_path.mkdir(parents=True)
+
+emojis_images_export_path = emojis_export_path / "images"
+emojis_images_export_path.mkdir(parents=True)
+
+emoji_page = 0
+emoji_url = api_url + "/emoji"
+
+while True:
+    params = {"per_page":200, "page": emoji_page}
+
+    response = requests.get(emoji_url, headers=headers, params=params)
+    fail_if_not_ok(response, "Retrieving custom emoji list page " + str(emoji_page) + "failed")
+
+    emojis = response.json()
+
+    if len(emojis) <= 0:
+        break
+
+    emoji_page += 1
+
+    with (emojis_export_path / "emojis.json").open("a+", encoding="utf8") as f:
+        json.dump(emojis, f)
+
+    # Retrieve custom emoji images
+    for emoji in emojis:
+        emoji_image_url = emoji_url + "/" + emoji["id"] + "/image"
+        response = requests.get(emoji_image_url, headers=headers, stream=True)
+            
+        fail_if_not_ok(response, "Failed to retrieve image for emoji " + str(emoji["id"]))
+
+        with (emojis_images_export_path / emoji["id"]).open("wb") as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+
+
+# -------------------------------------------
 #  Export teams
 # -------------------------------------------
 
@@ -134,7 +175,7 @@ teams = response.json()
 teams_export_path = Path(export_path, "teams")
 teams_export_path.mkdir(parents=True)
 
-with (teams_export_path / "teams.json").open("w") as f:
+with (teams_export_path / "teams.json").open("w", encoding="utf8") as f:
     json.dump(teams, f)
 
 # -------------------------------------------
@@ -160,12 +201,12 @@ for team in teams:
     channels_url = api_url + "/users/me/teams/" + team["id"] + "/channels"
     response = requests.get(channels_url, headers=headers)
     
-    fail_if_not_ok(response, "Failed to channels for team " + team["name"])
+    fail_if_not_ok(response, "Failed to retrieve channels for team " + team["name"])
 
     channels = response.json()
 
     # Export channel definitions
-    with (team_export_path / "channels.json").open("w") as f:
+    with (team_export_path / "channels.json").open("w", encoding="utf8") as f:
         json.dump(channels, f)
 
     # Export each channel
@@ -180,7 +221,7 @@ for team in teams:
         fail_if_not_ok(response, "Failed to retrieve members for channel " + channel["name"])
 
         members = response.json()
-        with (channel_export_path / "members.json").open("w") as f:
+        with (channel_export_path / "members.json").open("w", encoding="utf8") as f:
             json.dump(members, f)
 
         # Export pinned posts
@@ -190,7 +231,7 @@ for team in teams:
         fail_if_not_ok(response, "Failed to retrieve pinned posts for channel " + channel["name"])
 
         pinned_posts = response.json()
-        with (channel_export_path / "pinned-posts.json").open("w") as f:
+        with (channel_export_path / "pinned-posts.json").open("w", encoding="utf8") as f:
             json.dump(pinned_posts, f)
 
         # Export all posts
@@ -210,17 +251,18 @@ for team in teams:
             fail_if_not_ok(response, "Failed to retrieve posts for page " + str(page) + " for channel " + channel["name"])
 
             posts = response.json()
+            posts_list = posts["posts"].values()
 
-            if len(posts) <= 0:
+            if len(posts_list) <= 0:
                 break
             
-            with (channel_export_path / ("posts-page-" + str(page) + ".json")).open("w") as f:
+            with (channel_export_path / ("posts-page-" + str(page) + ".json")).open("w", encoding="utf8") as f:
                 json.dump(posts, f)
 
             page += 1
 
             # Export all attachments
-            file_ids = [p["file_ids"] for p in posts["posts"].values() if "file_ids" in p]
+            file_ids = [p["file_ids"] for p in posts_list if "file_ids" in p]
             file_ids = [id for file_id_list in file_ids for id in file_id_list]
 
             for file_id in file_ids:
@@ -246,23 +288,3 @@ for team in teams:
                 with (thumbnails_path / file_id).open("wb") as f:
                     for chunk in response.iter_content(1024):
                         f.write(chunk)
-
-# teams_url = api_url + "/users/" + str(user_id) + "/teams"
-# response = requests.get(teams_url, headers=headers)
-# fail_if_not_ok(response, "Retrieving teams for user " + str(user) + "failed")
-
-# teams = response.json()
-# teams_html = "<html><head><title>Mattermost Export - Select Team</title></head><body><h2>Select team</h2><ul>"
-
-# for team in teams:
-#     teams_html += '<li><a href="' + team["id"] + '/index.html">' + team["display_name"] + '</a></li>'
-
-# teams_html += "</body></html>"
-
-# export_path = Path(output, "Mattermost-Export-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-# export_path.mkdir(parents=True)
-
-# index_file_path = export_path / "index.html"
-
-# with index_file_path.open("w", encoding ="utf-8") as f:
-#     f.write(teams_html)
